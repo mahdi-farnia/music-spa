0707b84d0d94d6636323ab42e05be03bee4771ee
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2023-02-02 13:38:11 +0330

    chore(Navbar): move navbar points to MainView folder!

--- src/components/MainView/index.tsx
+++ src/components/MainView/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react';
-import Navbar from 'components/Navbar';
+import Navbar from 'components/MainView/Navbar';
 import s from './style.module.css';
 
 const MainView: React.FC = () => (
